62aa7b2adc7b169df793894cc6e758f595925c42
Siddhanta <siddhanta.c@gmail.com>
2022-07-23 12:54:17 +0800

    wip

--- transaction.py
+++ transaction.py
@@ -11,6 +11,12 @@ class Transaction:
         self.date = date
         self.desc = desc
         self.account1 = account
-        self.amount = amount
+        # self.amount = amount
+        amt = str.split(amount, '$')[-1]
+        print(amt) 
+        print(float(str(amt)))
+        # if amt1 < 0:
+        #     print("NEGATIVE: "+amount)
+
     def __str__(self):
         return 'id='+self.txnid+', date='+self.date+', desc='+self.desc+', account1='+self.account1+', account2='+self.account2+', amount='+str(self.amount)
